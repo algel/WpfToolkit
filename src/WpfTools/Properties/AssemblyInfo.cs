1cfe38abede1f2f5f43b857f83d8a2cd0ef3a5b1
Alexey Pozdnyakov <algel.fam@gmail.com>
2017-12-19 14:54:22 +0300

    increase version

--- src/WpfTools/Properties/AssemblyInfo.cs
+++ src/WpfTools/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.12.18.0")]
-[assembly: AssemblyFileVersion("2017.12.18.0")]
+[assembly: AssemblyVersion("2017.12.19.1153")]
+[assembly: AssemblyFileVersion("2017.12.19.1153")]
