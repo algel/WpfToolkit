4c469e79c0659e97b64b228d74d6bf56ec59d893
Alexey Pozdnyakov <algel.fam@gmail.com>
2017-12-22 14:52:00 +0300

    refactor BindingDecoratorBase

--- src/WpfTools/Windows/Data/BindingDecoratorBase.cs
+++ src/WpfTools/Windows/Data/BindingDecoratorBase.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Globalization;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Markup;
+using System.Xaml;
 using JetBrains.Annotations;
+using XamlParseException = System.Windows.Markup.XamlParseException;
 
 namespace Algel.WpfTools.Windows.Data
 {
@@ -266,7 +270,28 @@ namespace Algel.WpfTools.Windows.Data
         /// </returns>
         /// <param name="provider">Object that can provide services for the markup
         /// extension.</param>
-        public override object ProvideValue(IServiceProvider provider)
+        public sealed override object ProvideValue(IServiceProvider provider)
+        {
+            CheckCanReceiveMarkupExtension(this, provider, out var targetDependencyObject, out var targetDependencyProperty);
+            if (targetDependencyObject == null || targetDependencyProperty == null)
+                return this;
+
+            
+            //// Retrieve target information
+            //var target = serviceProvider.GetService<IProvideValueTarget>();
+            //if (target?.TargetObject != null)
+            //{
+            //    // In a template the TargetObject is a SharedDp (internal WPF class)
+            //    // In that case, the markup extension itself is returned to be re-evaluated later
+            //    if (target.TargetObject.GetType().FullName == "System.Windows.SharedDp")
+            //        return this;
+            //}
+
+
+            return ProvideValueInternal(provider);
+        }
+
+        protected virtual object ProvideValueInternal(IServiceProvider provider)
         {
             // workaround:  .Net 4, that the serviceProvider dose not get the correct target property.
             var target = provider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
@@ -307,6 +332,180 @@ namespace Algel.WpfTools.Windows.Data
             dp = service.TargetProperty as DependencyProperty;
             return target != null && dp != null;
         }
+
+        #region Imported from MS.Internal.Helper in PresentationFramework 
+
+        /// <summary>
+        /// Checks if the given IProvideValueTarget can receive
+        /// a DynamicResource or Binding MarkupExtension.
+        /// </summary>
+        internal static void CheckCanReceiveMarkupExtension(
+            MarkupExtension markupExtension,
+            IServiceProvider serviceProvider,
+            out DependencyObject targetDependencyObject,
+            out DependencyProperty targetDependencyProperty)
+        {
+            targetDependencyObject = null;
+            targetDependencyProperty = null;
+
+            IProvideValueTarget provideValueTarget = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+            if (provideValueTarget == null)
+            {
+                return;
+            }
+
+            object targetObject = provideValueTarget.TargetObject;
+
+            if (targetObject == null)
+            {
+                return;
+            }
+
+            Type targetType = targetObject.GetType();
+            object targetProperty = provideValueTarget.TargetProperty;
+
+            if (targetProperty != null)
+            {
+                targetDependencyProperty = targetProperty as DependencyProperty;
+                if (targetDependencyProperty != null)
+                {
+                    // This is the DependencyProperty case
+
+                    targetDependencyObject = targetObject as DependencyObject;
+                    Debug.Assert(targetDependencyObject != null, "DependencyProperties can only be set on DependencyObjects");
+                }
+                else
+                {
+                    MemberInfo targetMember = targetProperty as MemberInfo;
+                    if (targetMember != null)
+                    {
+                        // This is the Clr Property case
+                        PropertyInfo propertyInfo = targetMember as PropertyInfo;
+
+                        // Setters, Triggers, DataTriggers & Conditions are the special cases of
+                        // Clr properties where DynamicResource & Bindings are allowed. Normally
+                        // these cases are handled by the parser calling the appropriate
+                        // ReceiveMarkupExtension method.  But a custom MarkupExtension
+                        // that delegates ProvideValue will end up here (see Dev11 117372).
+                        // So we handle it similarly to how the parser does it.
+
+                        EventHandler<XamlSetMarkupExtensionEventArgs> setMarkupExtension
+                            = LookupSetMarkupExtensionHandler(targetType);
+
+                        if (setMarkupExtension != null && propertyInfo != null)
+                        {
+                            IXamlSchemaContextProvider scp = serviceProvider.GetService(typeof(IXamlSchemaContextProvider)) as IXamlSchemaContextProvider;
+                            if (scp != null)
+                            {
+                                XamlSchemaContext sc = scp.SchemaContext;
+                                XamlType xt = sc.GetXamlType(targetType);
+                                if (xt != null)
+                                {
+                                    XamlMember member = xt.GetMember(propertyInfo.Name);
+                                    if (member != null)
+                                    {
+                                        var eventArgs = new XamlSetMarkupExtensionEventArgs(member, markupExtension, serviceProvider);
+
+                                        // ask the target object whether it accepts MarkupExtension
+                                        setMarkupExtension(targetObject, eventArgs);
+                                        if (eventArgs.Handled)
+                                            return;     // if so, all is well
+                                    }
+                                }
+                            }
+
+                        }
+
+
+                        // Find the MemberType
+
+                        Debug.Assert(targetMember is PropertyInfo || targetMember is MethodInfo,
+                            "TargetMember is either a Clr property or an attached static settor method");
+
+                        Type memberType;
+
+                        if (propertyInfo != null)
+                        {
+                            memberType = propertyInfo.PropertyType;
+                        }
+                        else
+                        {
+                            MethodInfo methodInfo = (MethodInfo)targetMember;
+                            ParameterInfo[] parameterInfos = methodInfo.GetParameters();
+                            Debug.Assert(parameterInfos.Length == 2, "The signature of a static settor must contain two parameters");
+                            memberType = parameterInfos[1].ParameterType;
+                        }
+
+                        // Check if the MarkupExtensionType is assignable to the given MemberType
+                        // This check is to allow properties such as the following
+                        // - DataTrigger.Binding
+                        // - Condition.Binding
+                        // - HierarchicalDataTemplate.ItemsSource
+                        // - GridViewColumn.DisplayMemberBinding
+
+                        if (!typeof(MarkupExtension).IsAssignableFrom(memberType) ||
+                            !memberType.IsAssignableFrom(markupExtension.GetType()))
+                        {
+                            throw new XamlParseException($"A '{markupExtension.GetType().Name}' cannot be set on the '{targetMember.Name}' property of type '{targetType.Name}'. A '{markupExtension.GetType().Name}' can only be set on a DependencyProperty of a DependencyObject.");
+                        }
+                    }
+                    else
+                    {
+                        // This is the Collection ContentProperty case
+                        // Example:
+                        // <DockPanel>
+                        //   <Button />
+                        //   <DynamicResource ResourceKey="foo" />
+                        // </DockPanel>
+
+                        // Collection<BindingBase> used in MultiBinding is a special
+                        // case of a Collection that can contain a Binding.
+
+                        if (!typeof(BindingBase).IsAssignableFrom(markupExtension.GetType()) ||
+                            !typeof(Collection<BindingBase>).IsAssignableFrom(targetProperty.GetType()))
+                        {
+                            throw new XamlParseException($"A '{markupExtension.GetType().Name}' cannot be used within a '{targetProperty.GetType().Name}' collection. A '{markupExtension.GetType().Name}' can only be set on a DependencyProperty of a DependencyObject.");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // This is the explicit Collection Property case
+                // Example:
+                // <DockPanel>
+                // <DockPanel.Children>
+                //   <Button />
+                //   <DynamicResource ResourceKey="foo" />
+                // </DockPanel.Children>
+                // </DockPanel>
+
+                // Collection<BindingBase> used in MultiBinding is a special
+                // case of a Collection that can contain a Binding.
+
+                if (!typeof(BindingBase).IsAssignableFrom(markupExtension.GetType()) || !typeof(Collection<BindingBase>).IsAssignableFrom(targetType))
+                {
+                    throw new XamlParseException($"A '{markupExtension.GetType().Name}' cannot be used within a '{targetType.Name}' collection. A '{markupExtension.GetType().Name}' can only be set on a DependencyProperty of a DependencyObject.");
+                }
+            }
+        }
+
+        static EventHandler<XamlSetMarkupExtensionEventArgs> LookupSetMarkupExtensionHandler(Type type)
+        {
+            if (typeof(Setter).IsAssignableFrom(type))
+                return Setter.ReceiveMarkupExtension;
+
+            if (typeof(DataTrigger).IsAssignableFrom(type))
+                return DataTrigger.ReceiveMarkupExtension;
+
+            if (typeof(Condition).IsAssignableFrom(type))
+                return Condition.ReceiveMarkupExtension;
+
+            return null;
+        }
+
+
+        #endregion
 
     }
 }
